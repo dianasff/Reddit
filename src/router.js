20950b3972ef19f5d6460242db896e14ba50b163
Diana Felix <dianasf.f@gmail.com>
2020-07-26 14:52:54 -0300

    Add global.css and router modification

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {BrowserRouter,Route, Switch} from 'react-router-dom';
 import Hot from './pages/Hot';
+import New from './pages/New';
+import Rising from './pages/Rising';
 
 
 export default function Routes(){
@@ -8,6 +10,9 @@ export default function Routes(){
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Hot}/>
+                <Route path='/hot' component={Hot}/>
+                <Route path='/new' component={New}/>
+                <Route path='/rising' component={Rising}/>
                 
             </Switch>   
         </BrowserRouter>
