518260e7e0e6b9d21265af0cb6d8b8d899c6b1bf
Diana Felix <dianasf.f@gmail.com>
2020-07-26 21:17:26 -0300

    Change  main route

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter,Route, Switch} from 'react-router-dom';
+import Principal from './pages/Main';
 import Hot from './pages/Hot';
 import New from './pages/New';
 import Rising from './pages/Rising';
@@ -9,7 +10,7 @@ export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path='/' exact component={Hot}/>
+                <Route path='/' exact component={Principal}/>
                 <Route path='/hot' component={Hot}/>
                 <Route path='/new' component={New}/>
                 <Route path='/rising' component={Rising}/>
